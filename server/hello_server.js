a28a8222d0abc23c077d7326cce4ffb253c3394c
hzlshen <m15277968105@163.com>
2017-12-03 23:30:27 +0800

    删掉重来

--- server/hello_server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by 神威如狱 on 2017/11/4.
- */
-var http = require('http');
-var server = http.createServer(function (require, response) {
-    response.end("Hello Node!");
-});
-server.listen(8000);
