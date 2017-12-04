27103e8cd8161c8e4573ae6f49a166c922746ac5
hzlshen <m15277968105@163.com>
2017-12-04 23:53:26 +0800

    npm install -g nodemon
    安装时时检测

--- build/auction_server.js
+++ build/auction_server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
 app.get("/products", function (req, res) {
     res.send("接收到商品查询请求");
 });
-var server = app.listen(4200, "localhost", function () {
+var server = app.listen(8000, "localhost", function () {
     console.log("服务器已启动，地址是：http://localhost:8000");
 });
