45db458ba7db5e341e23fba533e8972ec1dba268
hzlshen <m15277968105@163.com>
2017-12-05 00:06:41 +0800

    以json格式返回一个数组

--- build/auction_server.js
+++ build/auction_server.js
@@ -2,11 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
+var Product = (function () {
+    function Product(id, title, price, rating, desc, categories) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.rating = rating;
+        this.desc = desc;
+        this.categories = categories;
+    }
+    return Product;
+}());
+exports.Product = Product;
+var products = [
+    new Product(1, "第一个商品", 1.99, 2.5, "这是一个商品Helloo word", ["电子产品", "硬件设备"]),
+    new Product(2, "第二个商品", 8.49, 4.5, "这是二个商品Helloo word", ["图书", "硬件设备"]),
+    new Product(3, "第三个商品", 7.09, 1.5, "这是三个商品Helloo word", ["服装", "硬件设备"]),
+    new Product(4, "第四个商品", 6.39, 2.5, "这是四个商品Helloo word", ["线下", "硬件设备"]),
+    new Product(5, "第五个商品", 5.49, 2.5, "这是五个商品Helloo word", ["你好", "硬件设备"]),
+    new Product(6, "第六个商品", 3.69, 3.5, "这是六个商品Helloo word", ["电子产品", "硬件设备"])
+];
 app.get('/', function (req, res) {
     res.send("Hello Express");
 });
 app.get("/products", function (req, res) {
-    res.send("接收到商品查询请求");
+    res.json(products);
 });
 var server = app.listen(8000, "localhost", function () {
     console.log("服务器已启动，地址是：http://localhost:8000");
--- server/auction_server.ts
+++ server/auction_server.ts
@@ -2,12 +2,35 @@ import * as express from 'express';
 
 const app = express();
 
+export class Product{
+    constructor(
+        public id:number,
+        public title:string,
+        public price:number,
+        public rating:number,
+        public desc:string,
+        public categories:Array<string>
+    ){
+
+    }
+}
+
+const products:Product[]=[
+    new Product(1,"第一个商品",1.99,2.5,"这是一个商品Helloo word",["电子产品","硬件设备"]),
+    new Product(2,"第二个商品",8.49,4.5,"这是二个商品Helloo word",["图书","硬件设备"]),
+    new Product(3,"第三个商品",7.09,1.5,"这是三个商品Helloo word",["服装","硬件设备"]),
+    new Product(4,"第四个商品",6.39,2.5,"这是四个商品Helloo word",["线下","硬件设备"]),
+    new Product(5,"第五个商品",5.49,2.5,"这是五个商品Helloo word",["你好","硬件设备"]),
+    new Product(6,"第六个商品",3.69,3.5,"这是六个商品Helloo word",["电子产品","硬件设备"])
+];
+
+
 app.get('/',(req,res)=>{
     res.send("Hello Express");
 });
 
 app.get("/products",(req,res)=>{
-    res.send("接收到商品查询请求");
+    res.json(products);
 });
 
 const server = app.listen(8000,"localhost",()=>{
