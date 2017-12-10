3e58881c68b759b290864fc6db0e6f7e2c53ce25
hzlshen <m15277968105@163.com>
2017-12-10 23:03:12 +0800

    增加评论

--- build/auction_server.js
+++ build/auction_server.js
@@ -15,6 +15,18 @@ var Product = (function () {
     return Product;
 }());
 exports.Product = Product;
+var Comment = (function () {
+    function Comment(id, productId, timestamp, user, rating, content) {
+        this.id = id;
+        this.productId = productId;
+        this.timestamp = timestamp;
+        this.user = user;
+        this.rating = rating;
+        this.content = content;
+    }
+    return Comment;
+}());
+exports.Comment = Comment;
 var products = [
     new Product(1, "第一个商品", 1.99, 2.5, "这是一个商品Helloo word", ["电子产品", "硬件设备"]),
     new Product(2, "第二个商品", 8.49, 4.5, "这是二个商品Helloo word", ["图书", "硬件设备"]),
@@ -23,14 +35,27 @@ var products = [
     new Product(5, "第五个商品", 5.49, 2.5, "这是五个商品Helloo word", ["你好", "硬件设备"]),
     new Product(6, "第六个商品", 3.69, 3.5, "这是六个商品Helloo word", ["电子产品", "硬件设备"])
 ];
+var comments = [
+    new Comment(1, 1, "2017-02-20 22:13:22", "张三", 3, "东西不错"),
+    new Comment(2, 1, "2017-01-25 23:23:22", "李四", 4, "东西是不错"),
+    new Comment(3, 1, "2017-03-10 21:29:22", "王五", 2, "东西挺不错"),
+    new Comment(4, 2, "2017-04-06 20:22:22", "隔壁", 5, "东西可以不错"),
+];
 app.get('/', function (req, res) {
     res.send("Hello Express");
 });
+// 所有商品
 app.get("/api/products", function (req, res) {
     res.json(products);
 });
+// 商品 带id
 app.get("/api/product/:id", function (req, res) {
     res.json(products.find(function (product) { return product.id == req.params.id; }));
+});
+// 评论
+app.get("/api/product/:id/comments", function (req, res) {
+    //comments.filter((comment:Comment)=>comment.productId ==id);
+    res.json(comments.filter(function (comment) { return comment.productId == req.params.id; }));
 });
 var server = app.listen(8000, "localhost", function () {
     console.log("服务器已启动，地址是：http://localhost:8000");
--- server/auction_server.ts
+++ server/auction_server.ts
@@ -16,6 +16,19 @@ export class Product{
     }
 }
 
+export class Comment{
+    constructor(
+        public id :number,
+        public productId : number,
+        public timestamp:string,
+        public user: string,
+        public rating:number,
+        public content:string
+    ){
+
+    }
+}
+
 const products:Product[]=[
     new Product(1,"第一个商品",1.99,2.5,"这是一个商品Helloo word",["电子产品","硬件设备"]),
     new Product(2,"第二个商品",8.49,4.5,"这是二个商品Helloo word",["图书","硬件设备"]),
@@ -25,18 +38,32 @@ const products:Product[]=[
     new Product(6,"第六个商品",3.69,3.5,"这是六个商品Helloo word",["电子产品","硬件设备"])
 ];
 
+const comments:Comment[] = [
+    new Comment(1,1,"2017-02-20 22:13:22","张三",3,"东西不错"),
+    new Comment(2,1,"2017-01-25 23:23:22","李四",4,"东西是不错"),
+    new Comment(3,1,"2017-03-10 21:29:22","王五",2,"东西挺不错"),
+    new Comment(4,2,"2017-04-06 20:22:22","隔壁",5,"东西可以不错"),
+];
+
 
 app.get('/',(req,res)=>{
     res.send("Hello Express");
 });
 
-
+// 所有商品
 app.get("/api/products",(req,res)=>{
     res.json(products);
 });
 
+// 商品 带id
 app.get("/api/product/:id",(req,res)=>{
     res.json(products.find((product)=>product.id ==req.params.id));
+});
+
+// 评论
+app.get("/api/product/:id/comments",(req,res)=>{
+    //comments.filter((comment:Comment)=>comment.productId ==id);
+    res.json(comments.filter((comment:Comment)=>comment.productId ==req.params.id));
 });
 
 const server = app.listen(8000,"localhost",()=>{
